9e4d45ff6a1d654694f2941b80d668ee220b8f32
EHoTPoloskun <36607047+EHoTPoloskun@users.noreply.github.com>
2018-05-27 19:35:19 +0300

    Update snake.c

--- snake.c
+++ snake.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+#include <ncurses.h>
+#include <string.h>
 
+struct position_t {
+    int x;
+    int y;
+};
+
+struct snake_body_part_t {
+    struct position_t position;
+    struct snake_body_part_t *next;
+};
+
+enum direction_t {
+    LEFT,
+    UP,
+    RIGHT,
+    DOWN
+};
+
+struct food_t {
+    struct position_t position;
+    int value;
+};
+
+struct game_state_t {
+    int fieldWidth;
+    int fieldHeight;
+    struct snake_body_part_t *snake;
+    struct food_t *food;
+    enum direction_t direction;
+    enum direction_t blocked_direction;
+    int running;
+    int game_won;
+    WINDOW *win;
+};
